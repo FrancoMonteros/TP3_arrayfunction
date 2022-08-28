f6790a5ee00f1a2b13990a50608c7dde743c98a8
franco monteros <franco.n.monteros@gmail.com>
2022-08-28 19:37:15 -0300

    practico terminado

--- array/index.js
+++ array/index.js
@@ -1,10 +1,10 @@
 /*1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. 
 Mostrar por pantalla en forma de lista  los doce nombres del arreglo.*/
-let meses =["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
-let lista=1;
-for(let i=0; i<meses.length; i++){
-    
-    document.write("<br>" + lista + " Mes: "+meses[i]);
+let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+let lista = 1;
+for (let i = 0; i < meses.length; i++) {
+
+    document.write("<br>" + lista + " Mes: " + meses[i]);
     lista++;
 }
 document.write("<br>")
@@ -17,40 +17,40 @@ Mostrar en el documento web los ítems de las posiciones primera, tercera y últ
 Añade en última posición la ciudad de París.
 Escribe por pantalla el elemento que ocupa la segunda posición.
 Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.*/
-let ciudades=[];
+let ciudades = [];
 let agregar;
 
-    do{
-        agregar=prompt("Ingrese una ciudad ")
-        if(agregar){
-            ciudades.push(agregar);
-        }
-        
-    }while(confirm("deseas agragar otra ciudad?"))
+do {
+    agregar = prompt("Ingrese una ciudad ")
+    if (agregar) {
+        ciudades.push(agregar);
+    }
+
+} while (confirm("deseas agragar otra ciudad?"))
 
 
 document.write("<br>El array generado es:" + ciudades)
 document.write("<br>"[+ciudades.toString()]);
-document.write("<br>La longitud del arreglo es: "+ciudades.length);
-if(ciudades.length){
-    document.write("<br>La primera posicion es: "+ciudades[0]);
+document.write("<br>La longitud del arreglo es: " + ciudades.length);
+if (ciudades.length) {
+    document.write("<br>La primera posicion es: " + ciudades[0]);
 }
 
-if(ciudades.length){
-    document.write("<br>La tercera posicion es: "+ciudades[2]);
-    
+if (ciudades.length) {
+    document.write("<br>La tercera posicion es: " + ciudades[2]);
+
 }
 
-if(ciudades.length){
-    document.write("<br>La ultima posicion es: "+ciudades[ciudades.length-1]);
+if (ciudades.length) {
+    document.write("<br>La ultima posicion es: " + ciudades[ciudades.length - 1]);
 }
 document.write("<br>")
 ciudades.push("Paris");
 document.write(ciudades.toString());
 
-if(ciudades.length){
-    document.write("<br>La segunda posicion es: "+ciudades[1]);
-    
+if (ciudades.length) {
+    document.write("<br>La segunda posicion es: " + ciudades[1]);
+
 }
 document.write("<br>")
 ciudades.splice(1, 1, "Barcelona");
@@ -68,9 +68,9 @@ let suma;
 let resul_suma = [];
 let resul_gral = 0;
 
-for (i = 0; i < 50 ; i++) {
-    dados1 = (parseInt(Math.random() * 6) + 1);
-    dados2 = (parseInt(Math.random() * 6) + 1);
+for (i = 0; i < 50; i++) {
+    dados1 = (Math.floor(Math.random() * 6) + 1)
+    dados2 = (Math.floor(Math.random() * 6) + 1)
 
     suma = dados1 + dados2;
     resul_suma.push(suma);
--- function/index.js
+++ function/index.js
@@ -1,17 +1,58 @@
 /*1- Escribir el código de una función a la que se pasa como parámetro un número entero y 
 devuelve como resultado una cadena de texto que indica si el número es par o impar. 
 Mostrar por pantalla el resultado devuelto por la función.*/
+let parametro= parseInt(prompt("ingrese un parametro"));
+function par(numero) {
+  if (numero%2 === 0) {
+    return "el numero es par"
+  } else {
+    return "el numero es impar"
+  }
+}
+document.write("<br>"+ par(parametro));
+document.write("<br>")
 
 /*2- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. 
 A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, 
 sólo por minúsculas o por una mezcla de ambas.*/
+let texto2= prompt("ingrese un cadena de texto");
+function cadenatex(cadena) {
+ if (cadena === texto2.toUpperCase() ) {
+    return "la cadena está formada por mayúsculas"
+ }else if (cadena === texto2.toLowerCase() ) {
+   return "la cadena está formada por minúsculas"
+ }else {
+   return "la cadena está formada por ambas"
+ }
+}
+document.write(cadenatex(texto2));
+document.write("<br>")
 
 /*3- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, 
 luego crear una función para calcular su perímetro y mostrarlo por pantalla.
-
 La fórmula del perímetro  es p = 2*(a +b)*/
+let base = parseInt(prompt("numero de la base"));
+let altura = parseInt(prompt("numero de la altura"));
+function perimetro(altura , base) {
+  return 2 * (altura + base)
+  
+}
+document.write(perimetro(base, altura))
 
 /*4- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, 
 la creación de la tabla debe ser realizada con una función y 
 mostrar solo los resultados del 1 al 10 del número elegido por el usuario.*/
 
+let numero4= parseInt(prompt("ingrese un numero"));
+function tabla(tablamultiplicar) {
+  document.write("<table>")
+  for (let i = 1; i <= 10; i++) {
+    document.write("<tr>")
+    document.write("<td>"+numero4+ "</td>")
+    document.write("<td>"+ "*" +i+ "="+"</td>")
+    document.write("<td>"+i * tablamultiplicar+"</td>")
+    document.write("</tr>")
+  }
+  document.write("</table>")
+}
+tabla(numero4);
